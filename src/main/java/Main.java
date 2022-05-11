9c9e624fcfa2c71abcdc49b4f4274c4fe579ca04
Anton <antofffka@inbox.ru>
2022-05-11 23:27:56 +0300

    line metro print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,10 @@ import org.jsoup.select.Elements;
 public class Main {
     public static void main (String[] args) throws Exception {
         Document doc = Jsoup.connect("https://skillbox-java.github.io/").get();
-        Elements text = doc.select("name");
+        Element metrodata = doc.getElementById("metrodata");
+        Elements text = metrodata.getElementsByClass("js-metro-line");
         for(Element src : text){
-                System.out.println(src.text());
+                System.out.println(src.attr("data-line"));
         }
        // WriteJsonFile.writeJsonSimpleDemo("files/simlpe.json");
        // WriteJsonFile.writeJsonSimpleDemo("C://Users/Anton/IdeaProjects/javaWorkhome/ParseMetro/src/files/example.json");
